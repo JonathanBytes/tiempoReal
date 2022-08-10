c93babb4b303f71969563b803b8e1479b574a80d
Jonathan ArchDell <jonathancardonaramirez@gmail.com>
2022-08-10 07:21:43 -0500

    First commit

---
+++ primerosPasitos/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace primerosPasitos
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int numeroIngresado;
+            System.Console.Write("Ingrese un number: ");
+            numeroIngresado = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("El número ingresado es: " + numeroIngresado);
+        }
+    }
+}
